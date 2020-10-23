21e88e91d1c27b0f2dc9501d9ad771f2d2310468
emilinedoerr <edoerr@unomaha.edu>
2020-10-23 13:49:38 -0500

    Make SleepLog default URL

--- TechExercise/src/SleepLog.java
+++ TechExercise/src/SleepLog.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/SleepLog")
+@WebServlet("/")
 public class SleepLog extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
